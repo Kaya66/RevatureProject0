608fbc54bcde20fa9a4260798569b69e6fe69634
Kaya66 <zekezimmermanit@gmail.com>
2021-10-15 22:47:41 -0400

    Main landing page almost finished
    set up buttons and child/parent relationship with a module.
    
    Working on getting CSS for the child file finished.   (MUST FIX, CSS CHILD FILE. flavortext is everywhere)

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+//import { history } from 'my-originHistory';
 
-export default class App extends LightningElement {}
+
+export default class App extends LightningElement {
+    
+    @track parentValue="Second Value";
+
+    welcomeScreen = true;
+    handleClick(){
+        this.welcomeScreen = !this.welcomeScreen;
+    }
+}
---
+++ src/client/modules/my/originHistory/originHistory.js
@@ -0,0 +1,11 @@
+import { LightningElement, api } from 'lwc';
+
+export default class originHistory extends LightningElement{
+
+    @api 
+    originIntro = "Origin Jumpworks was founded in the mid-28th century, approx 2750. They produced High quality Fusion engines for the UEE military and mounted on the ostentatious Roberts Space Industires and Aegis Dynamics star yachts avaliable to the public at the time.";
+    origin300i = "TOrigin Jumpworks was founded in the mid-28th century, approx 2750. They produced High quality Fusion engines for the UEE military and mounted on the ostentatious Roberts Space Industires and Aegis Dynamics star yachts avaliable to the public at the time.hreeHundredEyeasdfa asdf asdf  dsf ds fa sdfasdfasd fsda g as g ad fa sdf asd g asdf asd fasd g sd gfas dg sad g sdaf asd f sdag sda fgasd f dasf asd f asdf asdf asd f asdf ad f adsf ";
+    origin600i = "Origin Jumpworks was founded in the mid-28th century, approx 2750. They produced High quality Fusion engines for the UEE military and mounted on the ostentatious Roberts Space Industires and Aegis Dynamics star yachts avaliable to the public at the time.";
+    origin890j = "Origin Jumpworks was founded in the mid-28th century, approx 2750. They produced High quality Fusion engines for the UEE military and mounted on the ostentatious Roberts Space Industires and Aegis Dynamics star yachts avaliable to the public at the time.";
+
+}
